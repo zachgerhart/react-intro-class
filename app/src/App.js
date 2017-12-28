686484dcf774f7ae4453611275732235b56926ce
ZachGerhart <zgerhart@gmail.com>
2017-12-28 11:59:45 -0600

    calculator

---
+++ app/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Calculator from './components/Calculator/Calculator.js'
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Calculator />
+      </div>
+    );
+  }
+}
+
+export default App;
